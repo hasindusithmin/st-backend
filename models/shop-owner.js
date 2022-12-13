f6d2a63520b1e8c18b3306affa6e8f534aafd3a2
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-13 12:37:42 +0530

    Create models/shop-owner.js

---
+++ models/shop-owner.js
@@ -0,0 +1,23 @@
+
+const {Schema,model} = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const shopOwnerSchema = new Schema({
+    first_name:String,
+    last_name:String,
+    email:String,
+    dob:String,
+    address:String,
+    contact:String,
+    place:String,
+    password:String
+})
+
+shopOwnerSchema.pre('save',async function(this){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
+})  
+
+const shopOwnerModel = model('shopowner',shopOwnerSchema)
+
+module.exports = shopOwnerModel;
