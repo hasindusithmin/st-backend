e75e0af0e7aa6573ba6be965d4bbe9a2504b81d3
Harshawalisundara <19it537@itum.mrt.ac.lk>
2023-01-08 13:22:31 +0000

    Update routes/travaller.js

--- routes/travaller.js
+++ routes/travaller.js
@@ -15,12 +15,44 @@ travallerRoute.get('/',async(req,res)=>{
     }
 })
 
+travallerRoute.get('/logout',async(req,res)=>{
+    try {
+        res.cookie('token','',{maxAge:0})
+        res.sendStatus(202)
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+})
+
+travallerRoute.get('/:id',async(req,res)=>{
+    try {
+        const {id} = req.params;
+        if (id == undefined) throw Error("id (params) is required.")
+        const travaller = await travallerModel.findById(id)
+        res.status(200).json(travaller)
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+})
+
 travallerRoute.post('/register',async(req,res)=>{
     try {
         const {first_name,last_name,email,date,address,phone_number,city,password} = req.body;
         const travaller = await travallerModel.create({first_name,last_name,email,date,address,phone_number,city,password})
         const tkn = jwt.sign({travaller},'jwt-secret',{expiresIn:'1h'})
         res.cookie('token',tkn)
+        res.status(200).json(travaller)
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+})
+
+travallerRoute.put('/:id',async(req,res)=>{
+    try {
+        const {id} = req.params;
+        if (id == undefined) throw Error("id (params) is required.")
+        const {first_name,last_name,email,date,address,phone_number,city,password} = req.body;
+        const travaller = await travallerModel.findByIdAndUpdate(id,{first_name,last_name,email,date,address,phone_number,city,password})
         res.status(200).json(travaller)
     } catch (error) {
         res.status(500).json({error:error.message})
@@ -39,14 +71,18 @@ travallerRoute.post('/login',async(req,res)=>{
     }
 })
 
-travallerRoute.get('/logout',async(req,res)=>{
+travallerRoute.delete('/:id',async(req,res)=>{
     try {
-        res.cookie('token','',{maxAge:0})
-        res.sendStatus(202)
+        const {id} = req.params;
+        if (id == undefined) throw Error("id (params) is required.")
+        const travaller = await travallerModel.findByIdAndDelete(id)
+        res.status(200).json(travaller)
     } catch (error) {
         res.status(500).json({error:error.message})
     }
 })
+
+
 
 module.exports = travallerRoute;
 
