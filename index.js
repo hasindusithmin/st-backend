8cf8dc082ff91304c56241b2acca8febd859e23f
Harshawalisundara <85169930+Harshawalisundara@users.noreply.github.com>
2023-01-11 09:53:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // install express with `npm install express` 
+
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -7,6 +8,10 @@ const shopOwnerRoute = require('./routes/shop-owner')
 const travallerRoute = require('./routes/travaller')
 const smartTravallerRoute = require('./routes/smart-travaller')
 const tripRoute = require('./routes/trip')
+const guiderRouter = require('./routes/guider')
+require('dotenv').config();
+
+
 
 const app = express()
 
@@ -20,13 +25,17 @@ app.use('/shop-owner',shopOwnerRoute)
 app.use('/traveller',travallerRoute)
 app.use('/smart-traveller',smartTravallerRoute)
 app.use('/trip',tripRoute)
+app.use('/guider',guiderRouter)
+ 
 
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL; 
+
+const PORT = process.env.PORT;
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DATABASE_URL)
     .then(()=>{
-        app.listen(3000,()=>{
+        app.listen(PORT,()=>{
             console.log('DB connect\nServer Running Port:3000');
         })
     })
